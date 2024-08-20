ccdd207fc7499d43b3fc47cf1ac970797ff23f56
abxh <83485102+abxh@users.noreply.github.com>
2024-08-21 01:43:23 +0200

    add basic tokenizer

--- calculator.hs
+++ calculator.hs
@@ -1,21 +1,54 @@
+#!/usr/bin/env runhaskell
+
+import Data.Char (isAlpha, isAlphaNum, isDigit)
+import Data.List (isPrefixOf)
 import System.IO (hFlush, isEOF, stdout)
 
+data Token
+  = IdentifierToken
+  | OperatorToken
+  | ParenthesisToken
+  deriving (Show, Eq)
+
+isIdentifierToken c
+  | isAlphaNum c || c == '_' = True
+  | otherwise = False
+
+isOperatorToken :: Char -> Bool
+isOperatorToken c
+  | c `elem` ['=', '+', '-', '*', '/', '^'] = True
+  | otherwise = False
+
+isParenthesisToken :: Char -> Bool
+isParenthesisToken c
+  | c `elem` ['(', ')'] = True
+  | otherwise = False
+
+tokenizeLine :: String -> Maybe [(Char, Token)]
+tokenizeLine "" = Just []
+tokenizeLine (x : xs)
+  | isIdentifierToken x = ((x, IdentifierToken) :) <$> tokenizeLine xs
+  | isParenthesisToken x = ((x, ParenthesisToken) :) <$> tokenizeLine xs
+  | isOperatorToken x = ((x, OperatorToken) :) <$> tokenizeLine xs
+  | otherwise = Nothing
+
+-- validateLine :: [(Char, Token)] -> (Bool, [String])
+-- validateLine l = (True, ["Success!"])
+
 eval :: String -> String
-eval = id
+eval l = show $ tokenizeLine l
 
 evalLoop :: IO ()
 evalLoop =
   do
     putStr "> "
     hFlush stdout
-
     done <- isEOF
     if done
       then pure ()
       else do
         line <- getLine
-        putStr $ eval line
-        putStr "\n"
+        putStrLn $ eval line
         evalLoop
 
 main :: IO ()
